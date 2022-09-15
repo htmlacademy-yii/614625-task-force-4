d5ff65a567292e33007dc935776071bc5b0dda02
unknown <Pruchkin-Maksim@yandex.ru>
2022-09-15 07:30:24 +0300

    говнокод

--- scr/exceptions/StatusException.php
+++ scr/exceptions/StatusException.php
@@ -2,5 +2,8 @@
 namespace TaskForce\exceptions;
 
 class StatusException extends TaskException{
-
+    const STATUSES = [self::STATUS_NEW, self::STATUS_CANCELED, self::STATUS_WORKING, self::STATUS_COMPLETED, self::STATUS_FAILED];
+    if(!in_array($this->status, STATUSES)){
+        echo 'sa';
+    }
 }
