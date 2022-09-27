3debd24c89bee3c1396288f5430b292ade0f599f
Apocalipso <pruchkin-maksim@yandex.ru>
2022-09-27 14:40:13 +0300

    потуги бекендера

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 require 'vendor/autoload.php';
 use TaskForce\models\Task;
+use TaskForce\importers\DataImporter;
 
 $task = new Task(Task::STATUS_COMPLETED, 1, 2);
+$data = new DataImporter("/data/categories.csv");
+
+print_r('<pre>');
+var_dump($data);
+print_r('</pre>');
+// $data->setFlags(DataImporter::READ_CSV);
+// foreach ($data as $row) {
+//     print_r('<pre>');
+//     print_r($row);
+//     print_r('</pre>');
+//     // list($animal, $class, $legs) = $row;
+//     // printf("A %s is a %s with %d legs\n", $animal, $class, $legs);
+// }
---
+++ scr/importers/DataImporter.php
@@ -0,0 +1,10 @@
+<?php
+namespace TaskForce\importers;
+use SplFileInfo;
+
+class DataImporter extends SplFileInfo{
+    public $filename;
+    // function __construct($filename){
+    //     $this->filename = $filename;
+    // }
+}
