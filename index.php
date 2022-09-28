98ba351748a066c787e212f7107643b2c50689a4
Apocalipso <pruchkin-maksim@yandex.ru>
2022-09-28 17:40:10 +0300

    потуги разработчика1

--- index.php
+++ index.php
@@ -4,16 +4,12 @@ use TaskForce\models\Task;
 use TaskForce\importers\DataImporter;
 
 $task = new Task(Task::STATUS_COMPLETED, 1, 2);
-$data = new DataImporter("/data/categories.csv");
 
-print_r('<pre>');
-var_dump($data);
-print_r('</pre>');
-// $data->setFlags(DataImporter::READ_CSV);
-// foreach ($data as $row) {
-//     print_r('<pre>');
-//     print_r($row);
-//     print_r('</pre>');
-//     // list($animal, $class, $legs) = $row;
-//     // printf("A %s is a %s with %d legs\n", $animal, $class, $legs);
-// }
+$data = new DataImporter();
+$data->getFileObject("\data\categories.csv");
+//$echo = $data->getFileObject("/data/categories.csv");
+
+//$file->setFlags(SplFileObject::READ_CSV);
+
+//var_dump($echo);
+
--- scr/importers/DataImporter.php
+++ scr/importers/DataImporter.php
@@ -1,10 +1,31 @@
 <?php
 namespace TaskForce\importers;
-use SplFileInfo;
+use SplFileObject;
 
-class DataImporter extends SplFileInfo{
-    public $filename;
-    // function __construct($filename){
-    //     $this->filename = $filename;
-    // }
+class DataImporter{
+    //нужен ряд функций:
+    //получение файла
+    //получение названия таблицы
+    //название колонок
+
+    //categories name, icon
+    //в таблицу категорий добавить поле
+
+    //cities name, lat, long
+
+    public function getFileObject($filename){
+        //(file_exists(dirname(dirname(__DIR__)) . $filename));
+
+        $file = new \SplFileObject(dirname(dirname(__DIR__)) . $filename);
+        $file->setFlags(\SplFileObject::READ_CSV);
+        foreach ($file as $row) {
+            print_r('<pre>');
+            var_dump($row);
+            print_r('</pre>');
+        }
+
+        // foreach ($file as $row) {
+        //     var_dump($row);
+        // }
+    }
 }
