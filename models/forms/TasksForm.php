2a77eb870edff36ee43bcbb7d02b9abfa17446d8
unknown <Pruchkin-Maksim@yandex.ru>
2022-10-11 07:26:21 +0300

    начал

---
+++ models/forms/TasksForm.php
@@ -0,0 +1,15 @@
+<?php 
+namespace app\models\forms;
+
+use Yii;
+use app\models\Categories;
+use app\models\Tasks;
+use yii\base\Model;
+use yii\db\ActiveQuery;
+
+class TasksForm extends Model
+{
+
+}
+
+
