ad22693b61a02ecb6fe8bd21a4d7b4242719cba3
unknown <Pruchkin-Maksim@yandex.ru>
2022-10-13 00:06:26 +0300

    в процессе модели формы

--- models/forms/TasksForm.php
+++ models/forms/TasksForm.php
@@ -9,7 +9,15 @@ use yii\db\ActiveQuery;
 
 class TasksForm extends Model
 {
+    public $category = [];
+    public $noExecutor = false;
+    public $period;
 
+    const ONE_HOUR = 1;
+    const DAY = 24;
+    const WEEK = 168;
+    //получить просто таски
+    //получить отфильтрованные таски
 }
 
 
