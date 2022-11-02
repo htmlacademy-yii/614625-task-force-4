bc5a9e449ccc9ab4f47db491ae5b0812c7c1b03d
unknown <Pruchkin-Maksim@yandex.ru>
2022-11-02 20:16:05 +0300

    дописал валидацию формы

--- models/forms/TaskCreateForm.php
+++ models/forms/TaskCreateForm.php
@@ -30,7 +30,14 @@ class TaskCreateForm extends Model
     public function rules()
     {
         return [
-
+            [['name', 'description', 'category'], 'required'],
+            ['name', 'string', 'min' => 4, 'max' => 122],
+            ['description', 'string', 'min' => 10,'max' => 255],
+            [['dateCompletion'], 'compare', 'compareValue' => date('Y-m-d'),'operator' => '>', 'type' => 'date',
+                'message' => 'Дата выполнения задания не может быть раньше текущей даты'],
+            ['category', 'exist', 'targetClass' => Categories::class,'targetAttribute' => ['category' => 'id']],
+            ['budget', 'integer', 'min' => 1],
+            [['files'], 'file'],
         ];
     }
 }
